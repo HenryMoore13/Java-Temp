6d0977c9757a53a7235e0c50337585df429bea9a
Henry Moore <hmoore@basecampcodingacademy.org>
2019-01-07 15:30:33 -0600

    I built a function that converts to kelvin.
    co-author-by: John <jmorgan@basecampcodingacademy.org>

--- Converter.java
+++ Converter.java
@@ -1,10 +1,25 @@
 public class Converter {
     Double magnitude;
-    // String unit;
+    // String unit;magnitude, unit
 
     public Converter(Double magnitude, String unit) {
         this.magnitude = normalizeToKelvin(magnitude, unit);
         // his.unit = unit;
+    }
+
+    private Double normalizeToKelvin(Double magnitude, String unit) {
+        switch(unit){
+            case "F":
+                return (magnitude - 32) * 5/9 + 273.15;
+            case "C":
+                return magnitude + 273.15;
+            case "K":
+                return magnitude;
+            case "R":
+                return magnitude * 5/9;
+            default:
+                return null;
+        }
     }
 
     public Double toFahrenheit() {
